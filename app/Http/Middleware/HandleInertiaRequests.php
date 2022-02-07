999a97da312313a6aca3202beddc8e449196be3b
Nisar Ahmed <nisar.ahmed.dev@gmail.com>
2022-02-08 00:39:24 +0500

    updated

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -46,8 +46,8 @@ class HandleInertiaRequests extends Middleware
                         'email' => $request->user()->email,
                         'owner' => $request->user()->owner,
                         'account' => [
-                            'id' => $request->user()->account->id,
-                            'name' => $request->user()->account->name,
+                            // 'id' => $request->user()->account->id,
+                            // 'name' => $request->user()->account->name,
                         ],
                     ] : null,
                 ];
